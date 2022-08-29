91b53aae8854fe41dd5705d535d296c41d694047
Andrei_Rohau <rohau.andrei@gmail.com>
2022-08-29 12:16:56 +0300

    master to begin with

--- src/main/java/org/learn/controller/MainController.java
+++ src/main/java/org/learn/controller/MainController.java
@@ -2,9 +2,6 @@ package org.learn.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.learn.jms.Destination;
-import org.learn.jms.Producer;
-import org.learn.jms.Publisher;
-import org.learn.jms.Sendable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,43 +9,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import static org.learn.jms.Destination.QUEUE;
+import static org.learn.jms.Destination.TOPIC;
 
 @Slf4j
 @RestController
 public class MainController {
     @Value("${spring.application.name}")
     private String springApplicationName;
-    @Value("${index.page}")
-    private String indexPage;
-
-    private Publisher publisher;
-    private Producer producer;
-
-    private Map<Destination, Sendable> mqMap = new HashMap<>();
 
     @Autowired
-    public MainController(Publisher publisher, Producer producer) {
-        this.publisher = publisher;
-        this.producer = producer;
-        mqMap.put(Destination.QUEUE, producer);
-        mqMap.put(Destination.TOPIC, publisher);
+    public MainController() {
     }
 
     @GetMapping("/status")
     public String status() {
-        final String logMessage = "${spring.application.name}=[" + springApplicationName + "].\n" + "Working!";
-        log.info(logMessage);
-        return logMessage;
+        log.info("CALLED: /status");
+        return "[" + springApplicationName + "]. " + "Working!";
+    }
+
+    @GetMapping("/info")
+    public String getInfo() {
+        log.info("CALLED: /info");
+        return "QUEUE:EMITTED/PROCESSED=[" + QUEUE.getEmitted() + "/" + QUEUE.getProcessed() + "] <br/> " +
+                "TOPIC:EMITTED/PROCESSED=[" + TOPIC.getEmitted() + "/" + TOPIC.getProcessed() + "] <br/> " +
+                QUEUE.getStringBuilder().toString();
     }
 
     @GetMapping("/{destination}/")
     public String sendMessageToDestination(@PathVariable Destination destination, @RequestParam String message, @RequestParam boolean isPersistent) {
-        final String logMessage = "${spring.application.name}=[" + springApplicationName + "].\n" + "Message to "
-                + destination + "=[" + message + "]. Persistance=[" + isPersistent + "].";
-        log.info(logMessage);
-        mqMap.get(destination).sendMessage(message, isPersistent);
-        return logMessage + "\nSuccessful.";
+        log.info("[{}]." + "DEST=[{}]. MSG=[{}]. Persistence=[{}].", springApplicationName, destination, message, isPersistent);
+        return "sender.getClass().getSimpleName()" + " sent=[" + message + "].";
     }
 }
