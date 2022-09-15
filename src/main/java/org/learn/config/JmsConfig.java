95892523790b7fbfbc7a415a340521bf611974a8
arohau <arohau@edmunds.com>
2022-09-15 17:08:03 +0300

    upd

--- src/main/java/org/learn/config/JmsConfig.java
+++ src/main/java/org/learn/config/JmsConfig.java
@@ -79,8 +79,8 @@ public class JmsConfig {
         final Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, infrastructureConfig.getKafkaUrl());
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
-//        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-//        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 100);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 100);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, MsgDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
